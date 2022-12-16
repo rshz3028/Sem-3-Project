995cdaf8efdcc5605ca8ee5333ff347912d70915
Rishikesh S <61351936+rshz3028@users.noreply.github.com>
2022-12-16 13:55:44 +0530

    Update game_objs.py

--- game_objs.py
+++ game_objs.py
@@ -53,4 +53,22 @@ class Player(pygame.sprite.Sprite):
         if keys[K_DOWN]:
             self.y_change += PLAYER_SPEED
             self.facing = 'down'
-            
+
+
+class Block(pygame.sprite.Sprite):
+
+    def __init__(self,game,x,y):
+        self.game = game
+        self._layer = BLOCK_LAYER
+        self.groups = self.game.all_sprites,self.game.blocks 
+        pygame.sprite.Sprite.__init__(self,self.groups)
+
+        self.x = x*TILESIZE
+        self.y = y*TILESIZE
+
+        self.image = pygame.Surface([TILESIZE,TILESIZE])
+        self.image.fill(GREEN)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
